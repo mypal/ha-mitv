dce169d18475a92cbcc62907686881131e354fd5
shancheng.wang <shancheng.wang@dianping.com>
2020-03-10 12:13:18 +0800

    test

--- custom_components/mitv_state/binary_sensor.py
+++ custom_components/mitv_state/binary_sensor.py
@@ -4,14 +4,18 @@ from threading import Thread
 
 import time
 import voluptuous as vol
+from datetime import timedelta
 
 from homeassistant.const import CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.binary_sensor import (BinarySensorDevice, PLATFORM_SCHEMA, DEVICE_CLASS_POWER)
+from pymitv import Discover
 
 DEFAULT_NAME = "mitv state"
 
 _LOGGER = logging.getLogger(__name__)
+
+SCAN_INTERVAL = timedelta(seconds=5)
 
 # No host is needed for configuration, however it can be set.
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -27,8 +31,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     # If a hostname is set. Discovery is skipped.
     host = config.get(CONF_HOST)
     name = config.get(CONF_NAME)
-    # add_entities(MitvBinarySensor(tv, DEFAULT_NAME) for tv in Discover().scan())
-    add_entities([MitvBinarySensor(name, host)])
+    add_entities([MitvBinarySensor(name, host)], True)
 
 
 class MitvBinarySensor(BinarySensorDevice):
@@ -40,23 +43,16 @@ class MitvBinarySensor(BinarySensorDevice):
         self._host = host
         self._state = False
         self._sensor_type = DEVICE_CLASS_POWER
-        self._thread = QueryThread(self)
-        self._thread.start()
         _LOGGER.debug('%s(%s) registered' % (name, host))
 
-    def update(self, state):
-        self._state = state
-        self.schedule_update_ha_state()
+    def update(self):
+        self._state = bool(Discover().check_ip(self._host))
+        _LOGGER.debug('%s(%s) changes to %s' % (self.name, self.host, self._state))
 
     @property
     def device_class(self):
         """Return the class of this sensor."""
         return self._sensor_type
-
-    @property
-    def should_poll(self):
-        """No polling needed for a demo binary sensor."""
-        return False
 
     @property
     def name(self):
@@ -67,26 +63,3 @@ class MitvBinarySensor(BinarySensorDevice):
     def is_on(self):
         """Return true if the binary sensor is on."""
         return self._state
-
-    @property
-    def host(self):
-        return self._host
-
-
-class QueryThread(Thread):
-    def __init__(self, mitv: MitvBinarySensor):
-        super().__init__()
-        self._mitv = mitv
-        self._delay = 5
-
-    def run(self) -> None:
-        from pymitv import Discover
-        while True:
-            try:
-                new = Discover().check_ip(self._mitv.host)
-                _LOGGER.debug('%s\'s state is %s', (self._mitv.name, new))
-                if new != self._mitv.is_on:
-                    self._mitv.update(new)
-            except Exception:
-                _LOGGER.debug('exception occurred: %s' % str(Exception))
-            time.sleep(self._delay)
